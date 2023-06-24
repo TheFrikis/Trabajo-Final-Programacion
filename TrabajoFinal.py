dec650a8799bed9437d7bd70d738dda9d6f81ff0
Pritorres7 <134165539+Pritorres7@users.noreply.github.com>
2023-06-23 21:57:36 -0300

    Codigo Torres Priscila

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -20,3 +20,22 @@ match eleccion:
         print("Usted a seleccionado salir")
     case _:
         print("Ha seleccionado una opcion incorrecta")                
+
+def buscar_por_palabra_clave():
+        palabraClave = str(input("Ingrese la palabra clave de la ley: "))
+        if(len(palabraClave) > 0):         
+            consulta = "SELECT * FROM Leyes WHERE PalabraClave LIKE '{palabraClave}'".format(palabraClave)
+            result= db.ejecutar_conexion(consulta)
+        for data in result:
+            print(""" 
+                +NÚMERO DE REGISTRO :        {}
+                +TIPO DE NORMATIVA :    {}
+                +NÚMERO DE NORMATIVA :     {}
+                +FECHA DE CREACIÓN :     {}
+                +DESCRPCIÓN :     {}
+                +CATEGORIA :     {}
+                +JURISDICCIÓN :     {}
+                +ÓRGANO LEGISLATIVO :     {}
+                +PALABRA CLAVE :     {}
+                    
+                """.format(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8], data[9]))
